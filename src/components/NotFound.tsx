dbac121957f622ee202e7f246c99ef3f81a4b476
itsmeenavi <105837329+itsmeenavi@users.noreply.github.com>
2025-08-29 23:36:25 +0800

    UPDATE: Error BUild

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { FaHome, FaArrowLeft, FaCode, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const NotFound = () => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
